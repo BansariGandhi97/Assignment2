ce94665944345ff54a0c97ad7904f9143626b64a
Bansari Gandhi <bgandhi@depaul.edu>
2021-09-21 20:28:40 -0400

    Task2 Completed:added Player class

---
+++ Player.java
@@ -0,0 +1,11 @@
+package Assignment2;
+
+public class Player {
+    public int numPalyers=7;
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Player p1=new Player();
+        System.out.println("Number of players are :"+ p1.numPalyers);
+    }
+}
